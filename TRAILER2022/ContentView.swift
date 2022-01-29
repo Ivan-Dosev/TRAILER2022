c3d5744fa488112f81463ca85e96098acdc1f080
Ivan Dimitrov <biobeehive@abv.bg>
2022-01-29 05:44:38 +0200

    Initial Commit

---
+++ TRAILER2022/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TRAILER2022
+//
+//  Created by Dosi Dimitrov on 29.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TRAILER2022/TRAILER2022App.swift
@@ -0,0 +1,17 @@
+//
+//  TRAILER2022App.swift
+//  TRAILER2022
+//
+//  Created by Dosi Dimitrov on 29.01.22.
+//
+
+import SwiftUI
+
+@main
+struct TRAILER2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
